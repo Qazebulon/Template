85ae8e0220166adedf612d6a1f2468e24875ab85
Qazebulon <jonathan@lougeek.com>
2017-08-18 08:47:44 -0500

    added debug message

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -3,25 +3,41 @@ import { Component } from '@angular/core';
 //import {Component} from '@angular/core';
 import {Deploy} from '@ionic/cloud-angular';
 
+import {Platform} from 'ionic-angular';
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor(public deploy: Deploy) {
 
-    this.deploy.check().then((snapshotAvailable: boolean) => {
-      if (snapshotAvailable) {
-        // When snapshotAvailable is true, you can apply the snapshot
+  debugMsg: string = "init value";
 
-        console.log("SNAPSHOT AVAIABLE!");
+  constructor(public deploy: Deploy, public platform: Platform) {
+    this.debugMsg = "Constructor: starting point.";
+    console.log(this.debugMsg);
 
-        this.deploy.download().then(() => {
-          console.log("Downloaded :)");
-          return this.deploy.extract();
-        });
-      }
-    });
+    if(this.platform.is('ios')){
+
+      this.deploy.check().then((snapshotAvailable: boolean) => {
+        if (snapshotAvailable) {
+          // When snapshotAvailable is true, you can apply the snapshot
+
+          this.debugMsg = "SNAPSHOT AVAIABLE!";
+          console.log(this.debugMsg);
+
+          this.deploy.download().then(() => {
+            this.debugMsg = "Downloaded :)";
+            console.log(this.debugMsg);
+            return this.deploy.extract();
+          });
+        }
+      });
+
+    }else{
+      this.debugMsg = "Not iOS";
+      console.log(this.debugMsg);
+    }
 
   }
 }
