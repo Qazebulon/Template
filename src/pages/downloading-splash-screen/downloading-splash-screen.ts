2c0657d5a067ff0d324b6a3a5243a848f77e70ea
Qazebulon <jonathan@lougeek.com>
2017-08-29 16:32:32 -0500

    testing

--- src/pages/downloading-splash-screen/downloading-splash-screen.ts
+++ src/pages/downloading-splash-screen/downloading-splash-screen.ts
@@ -1,26 +1,9 @@
 import { Component } from '@angular/core';
 
-//import {Component} from '@angular/core';
-//import {Deploy} from '@ionic/cloud-angular';
-
-import {Platform} from 'ionic-angular';
-
 @Component({
-  selector: 'page-live-updated-content',
-  templateUrl: 'live-updated-content.html'
+  selector: 'page-downloading-splash-screen',
+  templateUrl: 'downloading-splash-screen.html'
 })
-export class LiveUpdatedContent {
-
-  debugMsg: string = "init value (live-updated-content):";
-
-  constructor(public platform: Platform) {
-    this.debugMsg += " - Constructor: starting point.";
-
-    if(this.platform.is('ios')){
-      this.debugMsg += " - ios device";
-    }else{
-      this.debugMsg += " - Not iOS";
-    }
-
-  }
+export class DownloadingSplashScreen {
+  constructor() {}
 }
